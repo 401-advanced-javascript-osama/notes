5907b5d42e016dc60adaa6b55e0a151175c821b4
osamamousa204 <osamamousa204@gmail.com>
2020-05-18 00:45:12 +0300

    created a README file

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 const Input = require('./lib/input')
 
 const Notes = require('./lib/notes')
-// Input()
+
 
 let createNoteObject = new Input();
-// console.log(myNote);
+
 let newNote = new Notes(createNoteObject);
 
 newNote.execute(createNoteObject);
